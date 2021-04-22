fdb00f269a00e6aa5049373f0aa42f0501155601
atayalin <56723545+atayalin@users.noreply.github.com>
2021-04-22 21:57:06 +0300

    Update AVG.asm

--- AVG.asm
+++ AVG.asm
@@ -32,21 +32,21 @@ main:	li $v0, 4
 	syscall
 
 # Passing arguments are "$a1" for array and "$a2" for size.
-# Return value is v1.
+# Return register is $v1.
 # Returns the average value of the array of elements.
-avg:	la   $a1, arr	
+avg:	la   $a1, arr				# Function head. (Assigning arguments.)
 	lw   $a2, size	 
 	addi $t0, $t0, 0 			# Assigning values.
 	
-	avg_F: 	beq  $t0, $a2, avg_FD		# If iterator $t0 is same with size $a2
+	avg_F: 	beq  $t0, $a2, avg_FD		# If iterator $t0 is same with size $a2.
 		lw   $t1, 0($a1)		
 		add  $v1, $v1, $t1		# Add a[i] to $v1.
 		addi $t0, $t0, 1		# Increase iterator.
 		addi $a1, $a1, 4		# Increase array pointer.
-		j   avg_FD			# Jump beginning.
+		j   avg_FD			# Jumps to beginning of the function.
 
 	avg_FD:
 		mul $t1, $a2, 4			
-		sub $a1, $a1, $t1		# Go to beginning position of array.
-		div $v1, $v1, $a2		# Calculate average value.
-		jr  $ra				# Jump to return address.
+		sub $a1, $a1, $t1		# Goes to beginning position of array.
+		div $v1, $v1, $a2		# Calculatess average value.
+		jr  $ra				# Jumps to return address.
